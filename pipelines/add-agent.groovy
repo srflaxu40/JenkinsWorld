8b0cc43cf4d312afe4bcc0dc7a178825f5c98b86
John <knepperjm@gmail.com>
2018-05-20 12:58:50 -0700

    Update add-agent.groovy

--- pipelines/add-agent.groovy
+++ pipelines/add-agent.groovy
@@ -26,7 +26,7 @@ def addNode (String AgentHome, String AgentExecutors, String AgentDescription, S
             "any",             // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
             AgentName,                         // Labels
             "",         // Launch strategy, JNLP is the Java Web Start setting services use
-            RetentionStrategy.INSTANCE) // Is the "Availability" field and INSTANCE means "Always"
+            "Always") // Is the "Availability" field and INSTANCE means "Always"
 
     Jenkins.instance.addNode(dumb)
     println "Agent '$it' created with $agentExecutors executors and home '$agentHome'"
